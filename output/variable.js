8d9c0fa2fa0636b89a3d20fce077e4a91587d19f
Rahul171201 <rahulroydps@gmail.com>
2023-01-19 10:31:26 +0530

    varibales file added

---
+++ output/variable.js
@@ -0,0 +1,6 @@
+"use strict";
+let num = 5;
+num = 6;
+const str = 'rahul';
+console.log(num);
+console.log(str);
---
+++ src/variable.ts
@@ -0,0 +1,7 @@
+let num : number = 5;
+num = 6;
+
+const str : string = 'rahul';
+
+console.log(num);
+console.log(str);
