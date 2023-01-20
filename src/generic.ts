ebc73d4d391ef303fea743651724389ca8f9a0d8
Rahul171201 <rahulroydps@gmail.com>
2023-01-20 16:32:52 +0530

    updated generics

--- src/generic.ts
+++ src/generic.ts
@@ -18,4 +18,56 @@ function convert_list_to_dictionary <T>(users : T[]) {
 let dict = convert_list_to_dictionary(user_data);
 console.log(dict);
 
+// Generics 
+interface Dict<T>{
+    [key : string] : T
+}
 
+function mapDict<T, U>(input : Dict<T>,
+    mappingCb: (arg : T) => U
+    ) : Dict<U>{
+
+    let toReturn : Dict<U> = {};   
+
+    for(let key in input){
+        toReturn[key] = mappingCb(input[key]);
+    }
+    return toReturn;
+}
+
+function filterDict <T>(input: Dict<T>,
+    filterCb : (arg : T) => boolean
+    ) : Dict<T>{
+    
+    let toReturn : Dict<T> = {};
+
+    for(let key in input){
+        if(filterCb(input[key])){
+            toReturn[key] = input[key];
+        }
+    }
+
+    return  toReturn;
+}
+
+function reduceDict <T, V>(input: Dict<T>,
+    reducerCb: (item : T, currentValue: V) => V,
+    initialValue: V 
+    ) : V{
+        
+    let toReturn : V = initialValue;
+
+    for(let key in input){
+        toReturn = reducerCb(input[key], toReturn);
+    }
+    return toReturn;
+}
+
+
+interface HasId{
+    id : string
+};
+
+function listToDict<T extends HasId>(list : T[]){
+
+}
